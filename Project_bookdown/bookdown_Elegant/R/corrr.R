ed66984ebfbb454c0c4e2d2c18eb14970235f548
perlatex <wangmj@sicnu.edu.cn>
2020-03-06 21:20:05 +0800

    del

--- Project_bookdown/bookdown_Elegant/R/corrr.R
+++
@@ -1,9 +0,0 @@
-# 学校层面的总统计 与 学习成绩
-# 相关系数矩阵
-library(corrr)
-df_complete2 %>%
-  ungroup() %>%
-  select(-school, -test_median_score) %>%
-  correlate() %>%
-  focus(test_mean_score) %>%
-  arrange(-test_mean_score)
