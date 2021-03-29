fd58ce635e9caf03b604ecdd7860a3ae16686870
perlatex <wangmj@sicnu.edu.cn>
2021-03-29 21:46:01 +0800

    modify yaml

--- edu_stats_report2022/workflow_render.R
+++ edu_stats_report2022/workflow_render.R
@@ -3,10 +3,14 @@
 # source(workflow_tidydata.R)         # -> "data/myData_df.Rdata"
 # source(workflow_tidydata_anon.R)    # -> "data/myData_df_anon.Rdata"
 
+# 问题与解决
 # run workflow_render.R within *.Rproj
 # 不知道为什么每次更新内容，再次运行render_book，都需要**Restarting R session**
 # 即 Session -> Restart R
 # 否则会报错：LaTeX Error: Environment tabu undefined.
+# 看<http://haozhu233.github.io/kableExtra/awesome_table_in_pdf.pdf>
+# 在index.Rmd文件yaml处加入\usepackage{tabu}\usepackage{colortbl}解决
+# 现在直接Source即可
 
 
 #######################################################
